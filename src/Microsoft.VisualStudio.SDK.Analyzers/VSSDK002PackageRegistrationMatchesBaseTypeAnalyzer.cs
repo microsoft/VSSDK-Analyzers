8752ea2d95b5bb0e58cbf4828c8d7de4eb670f52
Andrew Arnott <andrew.arnott@microsoft.com>
2025-01-27 17:06:48 -0700

    Fix analyzer diagnostic locality

--- src/Microsoft.VisualStudio.SDK.Analyzers/VSSDK002PackageRegistrationMatchesBaseTypeAnalyzer.cs
+++ src/Microsoft.VisualStudio.SDK.Analyzers/VSSDK002PackageRegistrationMatchesBaseTypeAnalyzer.cs
@@ -63,61 +63,51 @@ namespace Microsoft.VisualStudio.SDK.Analyzers
             context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.Analyze);
 
             // Register for compilation first so that we only activate the analyzer for applicable compilations
-            context.RegisterCompilationStartAction(compilationContext =>
+            context.RegisterCompilationStartAction(context =>
             {
-                INamedTypeSymbol? packageType = compilationContext.Compilation.GetTypeByMetadataName(Types.Package.FullName)?.OriginalDefinition;
-                INamedTypeSymbol? asyncPackageType = compilationContext.Compilation.GetTypeByMetadataName(Types.AsyncPackage.FullName)?.OriginalDefinition;
+                INamedTypeSymbol? packageType = context.Compilation.GetTypeByMetadataName(Types.Package.FullName)?.OriginalDefinition;
+                INamedTypeSymbol? asyncPackageType = context.Compilation.GetTypeByMetadataName(Types.AsyncPackage.FullName)?.OriginalDefinition;
+                INamedTypeSymbol? packageRegistrationType = context.Compilation.GetTypeByMetadataName(Types.PackageRegistrationAttribute.FullName);
                 if (packageType != null && asyncPackageType != null)
                 {
-                    // Reuse the type symbols we looked up so that we don't have to look them up for every single class declaration.
-                    compilationContext.RegisterSyntaxNodeAction(
-                        Utils.DebuggableWrapper(ctxt => this.AnalyzeClassDeclaration(ctxt, packageType, asyncPackageType)),
-                        SyntaxKind.ClassDeclaration);
+                    context.RegisterSymbolAction(
+                        Utils.DebuggableWrapper((SymbolAnalysisContext context) =>
+                        {
+                            INamedTypeSymbol namedTypeSymbol = (INamedTypeSymbol)context.Symbol;
+                            if (namedTypeSymbol.BaseType is null || !Utils.IsEqualToOrDerivedFrom(namedTypeSymbol.BaseType, packageType))
+                            {
+                                return;
+                            }
+
+                            AttributeData? packageRegistrationInstance = namedTypeSymbol.GetAttributes().FirstOrDefault(a => SymbolEqualityComparer.Default.Equals(a.AttributeClass, packageRegistrationType));
+                            if (packageRegistrationInstance is null)
+                            {
+                                return;
+                            }
+
+                            if (!(packageRegistrationInstance.NamedArguments.FirstOrDefault(kv => kv.Key == Types.PackageRegistrationAttribute.AllowsBackgroundLoading).Value.Value is bool allowsBackgroundLoading))
+                            {
+                                allowsBackgroundLoading = false;
+                            }
+
+                            bool isAsyncPackageBaseType = Utils.IsEqualToOrDerivedFrom(namedTypeSymbol.BaseType, asyncPackageType);
+                            if (isAsyncPackageBaseType != allowsBackgroundLoading)
+                            {
+                                // We have a problem. Use the syntax analyzer to find the location to report.
+                                var attributeSyntax = packageRegistrationInstance.ApplicationSyntaxReference?.GetSyntax(context.CancellationToken) as AttributeSyntax;
+                                if (attributeSyntax is object)
+                                {
+                                    AttributeArgumentSyntax? allowBackgroundLoadingSyntax = attributeSyntax.ArgumentList?.Arguments.FirstOrDefault(a => a.NameEquals?.Name?.Identifier.Text == Types.PackageRegistrationAttribute.AllowsBackgroundLoading);
+                                    Location location = allowBackgroundLoadingSyntax?.GetLocation() ?? attributeSyntax.GetLocation();
+                                    ImmutableDictionary<string, string?> properties = ImmutableDictionary.Create<string, string?>()
+                                        .Add(BaseTypeDiagnosticPropertyName, isAsyncPackageBaseType ? Types.AsyncPackage.TypeName : Types.Package.TypeName);
+                                    context.ReportDiagnostic(Diagnostic.Create(Descriptor, location, properties));
+                                }
+                            }
+                        }),
+                        SymbolKind.NamedType);
                 }
             });
-        }
-
-        private void AnalyzeClassDeclaration(SyntaxNodeAnalysisContext context, INamedTypeSymbol packageType, INamedTypeSymbol asyncPackageType)
-        {
-            var declaration = (ClassDeclarationSyntax)context.Node;
-            BaseTypeSyntax? baseType = declaration.BaseList?.Types.FirstOrDefault();
-            if (baseType == null)
-            {
-                return;
-            }
-
-            var baseTypeSymbol = context.SemanticModel.GetSymbolInfo(baseType.Type, context.CancellationToken).Symbol?.OriginalDefinition as ITypeSymbol;
-
-            if (Utils.IsEqualToOrDerivedFrom(baseTypeSymbol, packageType))
-            {
-                INamedTypeSymbol? packageRegistrationType = context.Compilation.GetTypeByMetadataName(Types.PackageRegistrationAttribute.FullName);
-                INamedTypeSymbol? userClassSymbol = context.SemanticModel.GetDeclaredSymbol(declaration, context.CancellationToken);
-                AttributeData? packageRegistrationInstance = userClassSymbol?.GetAttributes().FirstOrDefault(a => SymbolEqualityComparer.Default.Equals(a.AttributeClass, packageRegistrationType));
-                if (packageRegistrationInstance == null)
-                {
-                    return;
-                }
-
-                if (!(packageRegistrationInstance.NamedArguments.FirstOrDefault(kv => kv.Key == Types.PackageRegistrationAttribute.AllowsBackgroundLoading).Value.Value is bool allowsBackgroundLoading))
-                {
-                    allowsBackgroundLoading = false;
-                }
-
-                bool isAsyncPackageBaseType = Utils.IsEqualToOrDerivedFrom(baseTypeSymbol, asyncPackageType);
-
-                if (isAsyncPackageBaseType != allowsBackgroundLoading)
-                {
-                    var attributeSyntax = packageRegistrationInstance.ApplicationSyntaxReference?.GetSyntax(context.CancellationToken) as AttributeSyntax;
-                    if (attributeSyntax is object)
-                    {
-                        AttributeArgumentSyntax? allowBackgroundLoadingSyntax = attributeSyntax.ArgumentList?.Arguments.FirstOrDefault(a => a.NameEquals?.Name?.Identifier.Text == Types.PackageRegistrationAttribute.AllowsBackgroundLoading);
-                        Location location = allowBackgroundLoadingSyntax?.GetLocation() ?? attributeSyntax.GetLocation();
-                        ImmutableDictionary<string, string?> properties = ImmutableDictionary.Create<string, string?>()
-                            .Add(BaseTypeDiagnosticPropertyName, isAsyncPackageBaseType ? Types.AsyncPackage.TypeName : Types.Package.TypeName);
-                        context.ReportDiagnostic(Diagnostic.Create(Descriptor, location, properties));
-                    }
-                }
-            }
         }
     }
 }
